cd359a50023378188295e58a8d128248f6f91559
vanessa9711 <37115514+vanessa9711@users.noreply.github.com>
2019-05-14 16:38:02 -0500

    Clase Inicio

---
+++ ANALIZADOR_LEXICO2.py
@@ -0,0 +1,25 @@
+class Inicio:
+
+    """ ABRIR ARCHIVOS """
+    def abrir_archivo(self):
+        #Abrir .txt con lexico a analizar
+        expresiones = open("expresiones.txt")
+		
+        return expresiones
+
+    """ AGREGAR EL RESULTADO AL ARCHIVO """   
+    def escribir_archivo(self,resultado):
+        busquedas = open("resultados.txt", "w")
+        busquedas.write(resultado)
+        busquedas.close()   
+
+
+
+   
+
+
+
+
+       
+
+   
